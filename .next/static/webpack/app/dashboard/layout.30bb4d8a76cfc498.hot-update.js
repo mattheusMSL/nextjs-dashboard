20edbcb96124a35dd68c795560b38c78b5215cda
Matheus Santos Lopes <matthewpeterparker@gmail.com>
2024-02-07 01:11:21 -0300

    Add node_modules/ to .gitignore

---
+++ .next/static/webpack/app/dashboard/layout.30bb4d8a76cfc498.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/dashboard/layout",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fmatheuslopes%2FDocuments%2Fwww%2Fprojects%2Fnextjs%2Fdashboard%2Fnextjs-dashboard%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&server=false!":
+/*!************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fmatheuslopes%2FDocuments%2Fwww%2Fprojects%2Fnextjs%2Fdashboard%2Fnextjs-dashboard%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&server=false! ***!
+  \************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/link.js */ \"(app-pages-browser)/./node_modules/next/dist/client/link.js\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lMkZVc2VycyUyRm1hdGhldXNsb3BlcyUyRkRvY3VtZW50cyUyRnd3dyUyRnByb2plY3RzJTJGbmV4dGpzJTJGZGFzaGJvYXJkJTJGbmV4dGpzLWRhc2hib2FyZCUyRm5vZGVfbW9kdWxlcyUyRm5leHQlMkZkaXN0JTJGY2xpZW50JTJGbGluay5qcyZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/YjQ1ZCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIi9Vc2Vycy9tYXRoZXVzbG9wZXMvRG9jdW1lbnRzL3d3dy9wcm9qZWN0cy9uZXh0anMvZGFzaGJvYXJkL25leHRqcy1kYXNoYm9hcmQvbm9kZV9tb2R1bGVzL25leHQvZGlzdC9jbGllbnQvbGluay5qc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fmatheuslopes%2FDocuments%2Fwww%2Fprojects%2Fnextjs%2Fdashboard%2Fnextjs-dashboard%2Fnode_modules%2Fnext%2Fdist%2Fclient%2Flink.js&server=false!\n"));
+
+/***/ })
+
+});
